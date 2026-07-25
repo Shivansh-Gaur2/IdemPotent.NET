8660d66b245d7ec22e596430a783cf54c1931cd7
Shivansh-Gaur2 <shivanshgaur28@gmail.com>
2026-07-25 18:19:57 +0530

    - Created FingerprintCalculator - IdempotencyOptions - IIdempotencyStore to keep sql/redis store

---
+++ src/IdemPotent.Core/IdempotencyOptions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdemPotent.Core
+{
+    // Following the Options pattern, this class allows configuration of idempotency behavior in the application.
+    public class IdempotencyOptions
+    {
+        public string HeaderName { get; set; } = "Idempotency-Key";
+        public ConcurrentRequestStrategy ConcurrentRequestStratergy { get; set; } = ConcurrentRequestStrategy.Reject409;
+        public TimeSpan DefaultTtl { get; set; } = TimeSpan.FromHours(24);
+        public int MaxWaitSeconds { get; set; } = 5;
+        public int PollIntervalMs { get; set; } = 200;
+    }
+
+    public enum ConcurrentRequestStrategy
+    {
+        Reject409,
+        PollAndWait
+    }
+}
