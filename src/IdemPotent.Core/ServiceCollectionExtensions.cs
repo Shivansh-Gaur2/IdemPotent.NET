62ef80170effc29dc1e8088c3e0ddaec49f6d5fa
Shivansh-Gaur2 <shivanshgaur28@gmail.com>
2026-07-26 08:36:48 +0530

    - Added extendable methods to builder and app

---
+++ src/IdemPotent.Core/ServiceCollectionExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdemPotent.Core
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddIdempotency(this IServiceCollection services, Action<IdempotencyOptions>? configure = null)
+        {
+            var options = new IdempotencyOptions();
+            configure?.Invoke(options);
+
+            services.AddSingleton(options);
+
+            return services;
+        }
+
+        public static IApplicationBuilder useIdempotency(this IApplicationBuilder app)
+        {
+            return app.UseMiddleware<IdempotencyMiddleware>();
+        }
+    }
+}
